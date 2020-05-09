208aeb31f2434a2167b52be84d9c5f96cb54a484
Gedalya Krycer <gedalya@krycer.com>
2020-05-08 23:52:54 -0700

    Added the ability to open and close the modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,39 @@ console.log("hello world")
 // ——————————————————————————————————————————————————
 // ——————————————————————————————————————————————————
 
-$("#jokeBtn").on("click", function (response) {
-    console.log("Hello Joke Button!");
+// Closes model on background or close button click
+function closeModal() {
+    $("#modalClose").on("click", function () {
+        $(".modal").removeClass("is-active");
+    });
+    $(".modal-background").on("click", function () {
+        $(".modal").removeClass("is-active");
+    });
+};
+
+// Event Lister For Joke Button in Nav Bar
+$("#jokeBtn").on("click", function () {
+
+    // Adds a class to model to display it
+    $(".modal").addClass("is-active");
+
+    // Removes class to close Modal
+    closeModal();
+
+    // Joke API Url
+    var queryUrl = "https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous?type=single?blacklistFlags=nsfw+racist+sexist+religious+political"
+
+    // Code to work with API
+    $.ajax({
+        url: queryUrl,
+        method: "get"
+    }).then(function (response) {
+
+        console.log(response)
+    });
+
+    // Code to close the modal if the the x or background is clicked
+    // if ()
 });
 
 
